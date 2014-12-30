abe82dc88c114bd22e4547e793ec821e3a9023e6
ephraimrothschild@yahoo.com <ephraimrothschild@yahoo.com>
2014-12-30 09:03:14 -0500

    commit

---
+++ app/src/main/java/com/itc/crowd/MainActivity.java
@@ -0,0 +1,107 @@
+package com.itc.crowd;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import com.spotify.sdk.android.Spotify;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.authentication.SpotifyAuthentication;
+import com.spotify.sdk.android.playback.ConnectionStateCallback;
+import com.spotify.sdk.android.playback.Player;
+import com.spotify.sdk.android.playback.PlayerNotificationCallback;
+import com.spotify.sdk.android.playback.PlayerState;
+import com.spotify.sdk.android.playback.Config;
+
+
+public class MainActivity extends Activity implements PlayerNotificationCallback, ConnectionStateCallback{
+
+    private static final String CLIENT_ID = "4dd6a9c11dbf412d944b981abb0f55ab";
+    private static final String REDIRECT_URI = "crowd://callback";
+    private Player mPlayer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SpotifyAuthentication.openAuthWindow(CLIENT_ID, "token", REDIRECT_URI,
+                new String[]{"user-read-private", "streaming"}, null, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.d("test","started intent");
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+        if (uri != null) {
+            AuthenticationResponse response = SpotifyAuthentication.parseOauthResponse(uri);
+            Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+            Spotify spotify = new Spotify();
+            Log.d("test", "made spotify object");
+            mPlayer = spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+                @Override
+                public void onInitialized() {
+                    Log.d("test","begin initialized spotify player");
+                    mPlayer.addConnectionStateCallback(MainActivity.this);
+                    mPlayer.addPlayerNotificationCallback(MainActivity.this);
+                    mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
+                    Log.d("test","end initialized spotify player");
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onNewCredentials(String s) {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String s) {
+
+    }
+}
